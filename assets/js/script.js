b352322180afeefe069af9feb8a14f9bb2aaa511
Ruben Matamoros <ruben@valiantcreative.net>
2020-11-30 13:36:53 -0500

    created 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
 var cityInput = document.querySelector('#city-input');
 var cityBtn = document.querySelector('#search-btn');
-var forecastEl = document.querySelector('.city-forecast');
-var currentWeatherEl = document.querySelector('.current-weather');
-var cityNameEl = document.querySelector('.city-name');
 
 var formHandler = function(event) {
     event.preventDefault();
@@ -16,7 +13,7 @@ var formHandler = function(event) {
     }
 }
 
-// uses 'current weather api' to pull latitude and longitude
+// uses 'current weather api' to fetch latitude and longitude
 var getCoords = function(city) {
     var currentWeatherApi = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=91d7d61a609221126ce94454c32c2e3d`;
 
@@ -38,14 +35,15 @@ var getCoords = function(city) {
     })
 }
 
-    // uses latitude and longitude to pull current weather and five-day forecast
+    // uses latitude and longitude to fetch current weather and five-day forecast
     var getCityForecast = function(lon, lat) {
     var oneCallApi = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=91d7d61a609221126ce94454c32c2e3d`;
     fetch(oneCallApi).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                // fiveDayForecast(data);
+
                 currentForecast(data);
+                fiveDayForecast(data);
             });
         }
     })
@@ -53,21 +51,29 @@ var getCoords = function(city) {
 
 // formats and displays city name
 var formatCityName = function(city) {
+    var cityNameEl = document.querySelector('.city-name');
     var cityName = city.toLowerCase()
         .split(' ')
         .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
         .join(' ');
 
-    cityNameEl.textContent = `${cityName} at ${moment().format("h:mm A on M/D/YYYY")}`;
+        cityNameEl.textContent = `${cityName} (${moment().format("M/D/YYYY")})`;
 }
 
-// displays forecast for city
+// displays current forecast
 var currentForecast = function(forecast) {
 
+    var forecastEl = document.querySelector('.city-forecast');
     forecastEl.classList.remove('hide');
 
-    var currentTemp = forecast.current['temp'];
+    var currentTemp = parseInt(forecast.current['temp']);
     document.querySelector('#current-temp').textContent = currentTemp;
+
+    var currentHigh = parseInt(forecast.daily[0].temp.max);
+    document.querySelector('#current-high').textContent = currentHigh;
+
+    var currentLow = parseInt(forecast.daily[0].temp.min);
+    document.querySelector('#current-low').textContent = currentLow;
 
     var currentHumidity = forecast.current['humidity'];
     document.querySelector('#current-humidity').textContent = currentHumidity;
@@ -77,7 +83,7 @@ var currentForecast = function(forecast) {
 
     var uviSpan = document.querySelector('#current-uvi')
     var currentUvi = forecast.current['uvi'];
-    uviSpan.textContent = currentUvi
+    uviSpan.textContent = currentUvi;
 
     // styles UV index
     if (currentUvi <= 2) {
@@ -88,8 +94,49 @@ var currentForecast = function(forecast) {
         uviSpan.className = 'badge badge-danger';
     } else {
         uviSpan.className = 'badge text-light';
-        uviSpan.setAttribute('style', 'background-color: #553C7B')
+        uviSpan.setAttribute('style', 'background-color: #553C7B');
+    }
+}
+
+// displays five day forecast
+var fiveDayForecast = function(forecast) { 
+
+    for (var i = 1; i < 6; i++) {
+        var dateP = document.querySelector('#date-' + i);
+        dateP.textContent = moment().add(i, 'days').format('M/D/YYYY');
+
+        var tempP = document.querySelector('#temp-' + i);
+        tempP.textContent = `Temperature: ${parseInt(forecast.daily[i].temp.day)} °F`; 
+
+        var highP = document.querySelector('#high-' + i);
+        highP.textContent = `High: ${parseInt(forecast.daily[i].temp.max)} °F`;
+
+        var lowP = document.querySelector('#low-' + i);
+        lowP.textContent = `Low: ${parseInt(forecast.daily[i].temp.min)} °F`;
+
+        var humidityP = document.querySelector('#humidity-' + i);
+        humidityP.textContent = `Humidity: ${forecast.daily[i].humidity}%`;
     }
 }
 
 cityBtn.addEventListener('click', formHandler)
+
+
+//        /((((((\\\\
+//======((((((((((\\\\\
+//     ((           \\\\\\\
+//     ( (*    _/      \\\\\\\
+//       \    /  \      \\\\\\________________
+//        |  |   |       </                  ((\\\\
+//        o_|   /        /                      \ \\\\    \\\\\\\
+//             |  ._    (                        \ \\\\\\\\\\\\\\\\
+//            | /                       /       /    \\\\\\\     \\
+//     .______/\/     /                 /       /         \\\
+//    / __.____/    _/         ________(       /\
+//   / / / ________/`_________'         \     /  \_
+//  / /  \ \                             \   \ \_  \
+// ( <    \ \                             >  /    \ \
+//  \/     \\_                           / /       > )
+//          \_|                         / /       / /
+//                                    _//       _//
+//                                   /_|       /_|
